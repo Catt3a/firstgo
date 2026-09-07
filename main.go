b48e8471cef62ebcf8dd1d02963c07989a9eb847
Catt3a <169023485+Catt3a@users.noreply.github.com>
2026-09-07 21:09:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "привет")
+		fmt.Fprint(w, "привет")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("порт сервера: %s\n", port)
+	
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic(err)
+	}
 }
